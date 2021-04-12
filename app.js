2b3f1847b7df7785a2e7fb5df5e801a09418d4a4
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-04-12 16:45:14 +0430

    chore: ایجاد مسیریابی و صفحه اصلی
    به نحوه مسیریابی و ایجاد صفحه اصلی پرداخته شده تا دیگر صفخات هم
     به همین شکل ایجاد شوند

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+var reactViews = require('express-react-views');
+var routeIndex = require('./routes').index;
+const app = express();
+const port = 9000;
 
-app.get('/', (req, res) => {
-  res.send('Student Services')
-})
+
+//set static dir
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', reactViews.createEngine());
+
+app.get('/', routeIndex);
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
