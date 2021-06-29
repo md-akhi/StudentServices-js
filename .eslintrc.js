b47a78e7acd3f0dba069f0312084af89f4ab360a
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-06-29 19:51:36 +0430

    ci(eslint): بهبود تنظیمات

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+	commonjs: true,
+	es2020: true,
+	node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
   parserOptions: {
+	ecmaVersion: 11,
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["react"],
-  rules: {},
+  rules: {
+	  	indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"],
+	},
 };
