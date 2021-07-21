93b08ce8e73d5b43ddf67205ac24dd6f8fd6e7db
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-07-21 21:38:16 +0430

    chore: اصلاح session و تفکیک فایل پایگاه داده

---
+++ config/db.js
@@ -0,0 +1,18 @@
+let mongoose = require("mongoose");
+
+module.exports = function (env) {
+  //mongoDB
+  const dbUrl = env.MONGODB_URL || "mongodb://127.0.0.1:27017/studentServices";
+  mongoose.Promise = global.Promise;
+  mongoose
+    .connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    .catch((e) => {
+      console.error("Connection error", e.message);
+    });
+  mongoose.connection;
+  return mongoose;
+};
--- config/expers.js
+++ config/expers.js
@@ -1,76 +1,68 @@
+require("dotenv").config();
+let session = require("express-session");
+let MongoStore = require("connect-mongo");
+let cookieParser = require("cookie-parser");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+//let fs = require("fs");
+//let path = require("path");
+let logger = require("morgan");
+//let multer = require("multer");
+let compression = require("compression");
 
-require('dotenv').config();
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
-const mongoStore = require('connect-mongo');
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const fs = require("fs");
-const path = require("path");
-const logger = require("morgan");
-const multer = require("multer");
-const compression = require('compression');
+const pkg = require("../package.json");
+const env = process.env;
+const mode = env.NODE_ENV || "development";
+const port = env.PORT || 3000;
 
+module.exports = function (app, infoApp) {
+  let mongoose = require("./db.js")(env);
 
-const pkg = require('../package.json');
-const env = process.env.NODE_ENV || 'development';
-const port = process.env.PORT || 3000;
+  app.use(cookieParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
 
-//mongoDB
-const dbUrl = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/studentServices';
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  })
-  .catch((e) => {
-    console.error("Connection error", e.message);
+  // setup the logger
+  // Don't log during tests
+  // Logging middleware
+  if (mode !== "test") app.use(logger("dev"));
+
+  // Compression middleware (should be placed before express.static)
+  app.use(compression());
+
+  //if (app.get("env") === "production") {
+  app.set("trust proxy", 1);
+  let sessionStore = MongoStore.create({
+    mongoUrl: "mongodb://127.0.0.1:27017/studentServices",
   });
-const db = mongoose.connection;
+  app.use(
+    session({
+      secret: pkg.name,
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        path: "/",
+        maxAge: 7 * 24 * 60 * 60,
+        secure: true,
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60),
+      },
+      store: sessionStore,
+    })
+  );
+  //}
 
-
-module.exports = function(app, ssen) {
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
-// setup the logger
-// Don't log during tests
-// Logging middleware
-if (env !== 'test')
-  app.use(logger("dev"));
-
-// Compression middleware (should be placed before express.static)
-app.use(compression());
-
-// expose package.json to views
-app.use(function(req, res, next) {
-  res.locals.pkg = pkg;
-  res.locals.env = env;
-  next();
-});
-
-app.set("trust proxy", 1);
-app.use(
-  session({
-    secret: pkg.name,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 1 * 24 * 60 * 60,
-      secure: true,
-      expires: new Date(Date.now() + 1 * 24 * 60 * 60),
+  // expose package.json to views
+  app.use(function (req, res, next) {
+    if (infoApp.session === undefined) {
+      infoApp.session = req.session;
+      infoApp.session.login = false;
     }
-  })
-);
+    //else req.session = infoApp.session;
+    next();
+  });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
-
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
 };
