df60617d2d712b645e467e3ce7a297233a6b7ff9
MD Amanalikhani <md.akhi.ir@gmail.com>
2021-09-05 21:31:08 +0430

    perf(db): تفکیک اتصال پایگاه از فایل expres

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 let mongoose = require("mongoose");
 
 module.exports = function (env) {
-  //mongoDB
-  const dbUrl = env.MONGODB_URL || "mongodb://127.0.0.1:27017/studentServices";
-  mongoose.Promise = global.Promise;
-  mongoose
-    .connect(dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    .catch((e) => {
-      console.error("Connection error", e.message);
-    });
-  mongoose.connection;
-  return mongoose;
+	//mongoDB
+	const dbUrl =
+		env.MONGODB_URL + env.DB_NAME ||
+		"mongodb://127.0.0.1:27017/studentServices";
+	mongoose.Promise = global.Promise;
+	mongoose
+		.connect(dbUrl, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		})
+		.catch((e) => {
+			console.error("Connection error", e.message);
+		});
+	mongoose.connection;
+	return mongoose;
 };
