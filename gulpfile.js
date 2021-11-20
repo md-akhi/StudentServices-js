d8174cf37a4c1a67d02030d59c3eae99c83e0191
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-11-20 22:33:04 +0330

    ci(gulp): اصلاح تنظیمات

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,64 @@
-//require("dotenv").config();
 // Fetch required plugins
-const gulp = require("gulp");
-//const { src, dest, watch, series, parallel } = require("gulp");
-const imagemin = require("gulp-imagemin");
-const sourcemaps = require("gulp-sourcemaps");
-const concat = require("gulp-concat");
-const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
-const cssnano = require("cssnano");
-const uglify = require("gulp-uglify");
-const rename = require("gulp-rename");
-const del = require("delete");
-const infoPlugins = require("./build/npm/Plugins");
+const { src, dest, series, parallel } = require("gulp"),
+	//imagemin = require("gulp-imagemin"),
+	sourcemaps = require("gulp-sourcemaps"),
+	concat = require("gulp-concat"),
+	sass = require("gulp-sass")(require("sass")),
+	autoprefixer = require("gulp-autoprefixer"),
+	cssnano = require("gulp-cssnano"),
+	uglify = require("gulp-uglify"),
+	rename = require("gulp-rename"),
+	notify = require("gulp-notify"),
+	cache = require("gulp-cache"),
+	clean = require("gulp-clean"),
+	babel = require("gulp-babel"),
+	rollup = require("gulp-rollup"),
+	babelRU = require("rollup-plugin-babel"),
+	infoPlugins = require("./build/gulp/Plugins");
+
+//sass(require("sass"));
 
 // All paths
 const paths = {
-	images: {
-		src: ["./build/img/**/*"],
-		dest: "./public/img/",
-	},
+	// images: {
+	// 	src: ["./src/views/data/img/*"],
+	// 	dest: "./src/public/img/",
+	// },
 	styles: {
-		src: ["./build/scss/**/*.scss"],
-		dest: "./public/css/",
+		src: ["./src/views/data/scss/all.scss"],
+		dest: "./src/public/css/",
 		merge: "stylesheet",
 	},
 	scripts: {
-		src: ["./build/js/**/*.js"],
-		dest: "./public/js/",
+		src: ["./src/views/data/js/*.js"],
+		dest: "./src/public/js/",
 		merge: "javascript",
 	},
 	plugins: {
 		src: "/*",
-		dest: "./public/",
+		dest: "./src/public/plugins/",
 	},
 };
 
-function clean(cb) {
-	return del(["public/js/*.js", "public/css/*.css"], function (err, deleted) {
-		if (err) throw err;
-	});
-}
-
-// Optimize images(.png, .jpeg, .gif, .svg)
-/**
- * Custom options
- * imagemin([
- *       imagemin.gifsicle({ interlaced: true }),
- *       imagemin.mozjpeg({ quality: 75, progressive: true }),
- *       imagemin.optipng({ optimizationLevel: 5 }),
- *       imagemin.svgo({
- *         plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
- *       })
- *     ])
- */
-function images() {
-	return gulp
-		.src(paths.images.src)
-		.pipe(imagemin().on("error", (error) => console.log(error)))
-		.pipe(gulp.dest(paths.images.dest));
-}
+//Optimize images(.png, .jpeg, .gif, .svg)
+// const imagesTask = () => {
+// 	return gulp
+// 		.src(paths.images.src)
+// 		.pipe(
+// 			cache(
+// 				imagemin([
+// 					imagemin.gifsicle({ interlaced: true }),
+// 					imagemin.mozjpeg({ quality: 75, progressive: true }),
+// 					imagemin.optipng({ optimizationLevel: 3 }),
+// 					imagemin.svgo({
+// 						plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+// 					}),
+// 				])
+// 			).on("error", (error) => console.log(error))
+// 		)
+// 		.pipe(dest(paths.images.dest));
+// 	//.pipe(notify({ message: "Images task complete" }));
+// };
 
 // Compile styles
 /**
@@ -67,85 +67,108 @@ function images() {
  *
  * Note - Not all plugins work with postcss, only the ones mentioned in their documentation
  */
-function styles() {
-	return gulp
-		.src(paths.styles.src)
+const stylesTask = () => {
+	return src(paths.styles.src)
 		.pipe(sourcemaps.init())
-		.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-		.pipe(autoprefixer())
-		.pipe(cssnano())
+		.pipe(
+			sass({
+				style: "expanded",
+			}).on("error", sass.logError)
+		)
 		.pipe(concat(paths.styles.merge + ".css")) // So use gulp-rename to change the extension
+		.pipe(autoprefixer())
+		.pipe(dest(paths.styles.dest))
+		.pipe(cssnano())
 		.pipe(rename({ extname: ".min.css" }))
 		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest(paths.styles.dest));
-}
+		.pipe(dest(paths.styles.dest));
+	//.pipe(notify({ message: "Styles task complete" }));
+};
 
 // Minify scripts
 /**
  * To concat scripts, add below code after sourcemaps is initialized
  * .pipe(concat('{OutputFileName}.js'))
  */
-function scripts() {
-	return gulp
-		.src(paths.scripts.src)
-		.pipe(sourcemaps.init())
-		.pipe(uglify().on("error", (error) => console.log(error)))
-		.pipe(concat(paths.scripts.merge + ".js")) // So use gulp-rename to change the extension
-		.pipe(rename({ extname: ".min.js" }))
-		.pipe(sourcemaps.write("."))
-		.pipe(gulp.dest(paths.scripts.dest));
-}
+const scriptsTask = () => {
+	const pkg = require("./package");
+	const year = new Date().getFullYear();
+	return (
+		src(paths.scripts.src)
+			.pipe(sourcemaps.init())
+			.pipe(
+				rollup({
+					// any option supported by Rollup can be set here.
+					input: "./src/views/data/js/AdminLTE.js",
+					output: {
+						format: "umd",
+						name: paths.scripts.merge,
+						banner: `
+						/*!
+							* MD AKHI v${pkg.version} (${pkg.homepage})
+							* Copyright 2021-${year} ${pkg.authors[0]}
+							* Licensed  (https://github.com//LICENSE)
+							*/`,
+						jquery: "jQuery",
+					},
+					plugins: [
+						babelRU({
+							presets: ["@babel/preset-env"],
+							//exclude: "node_modules/**",
+							externalHelpers: true,
+						}),
+					],
+				})
+			)
+			// .pipe(
+			// 	babel({
+			// 		presets: ["@babel/preset-env"],
+			// 	})
+			// )
+			//.pipe(concat(paths.scripts.merge + ".js")) // So use gulp-rename to change the extension
+			.pipe(rename({ basename: paths.scripts.merge, extname: ".js" }))
+			.pipe(dest(paths.scripts.dest))
+			.pipe(uglify().on("error", (error) => console.log(error)))
+			.pipe(rename({ extname: ".min.js" }))
+			.pipe(sourcemaps.write("."))
+			.pipe(dest(paths.scripts.dest))
+	);
+	//.pipe(notify({ message: "Scripts task complete" }));
+};
 
 // copy plugins
-function plugins() {
-	infoPlugins.forEach((module) => {
-		gulp
-			.src(module.from + paths.plugins.src)
-			.pipe(gulp.dest(paths.plugins.dest + module.to));
+const pluginsTask = () => {
+	return infoPlugins.forEach((module) => {
+		src(module.from + paths.plugins.src, { allowEmpty: true }).pipe(
+			dest(paths.plugins.dest + module.to)
+		);
+		//.pipe(notify({ message: "Scripts task complete" }));;
 	});
-}
+};
 
-// Watch for file modification at specific paths and run respective tasks accordingly
-function watch() {
-	gulp.watch(paths.images.src, images);
-	gulp.watch(paths.styles.src, styles);
-	gulp.watch(paths.scripts.src, scripts);
-}
+// Clean
+const cleanTask = () => {
+	return src("src/public", { allowEmpty: true, read: false }).pipe(clean());
+};
 
-function development() {
-	return gulp.series(
-		clean,
-		gulp.parallel(images, styles, scripts, plugins),
-		watch
-	);
-}
+const developmentTask = () => {
+	return series(cleanTask, stylesTask, scriptsTask, pluginsTask);
+};
 
-function production() {
-	return gulp.series(
-		clean,
-		gulp.parallel(images, styles, scripts, plugins),
-		watch
-	);
-}
+// const productionTask = () => {
+// 	return series(cleanTask, stylesTask, scriptsTask, pluginsTask, imagesTask);
+// };
 
-// Export tasks to make them public
-exports.images = images;
-exports.styles = styles;
-exports.scripts = scripts;
-exports.plugins = plugins;
-exports.watch = watch;
-
-exports.development = development;
-exports.production = production;
-
-if (process.env.NODE_ENV === "production") {
-	exports.default = production;
-} else {
-	exports.default = development;
-}
+//if (process.env.NODE_ENV === "development") {
+// Default task
+exports.default = developmentTask();
+// } else {
+// 	// Default task
+// 	exports.default = productionTask();
+// }
 
 // const webpack = require("webpack-stream");
-// gulp.task("default", function () {
+// task("default", function () {
 // 	return gulp
 // 		.src("src/file.js")
 // 		.pipe(
@@ -153,126 +176,46 @@ if (process.env.NODE_ENV === "production") {
 // 				// Any configuration options...
 // 			})
 // 		)
-// 		.pipe(gulp.dest("public/"));
+// 		.pipe(dest("public/"));
 // });
 
 // gulpfile.js
 // Load plugins
-var gulp = require("gulp"),
-	sass = require("gulp-ruby-sass"),
-	autoprefixer = require("gulp-autoprefixer"),
-	minifycss = require("gulp-minify-css"),
-	jshint = require("gulp-jshint"),
-	uglify = require("gulp-uglify"),
-	imagemin = require("gulp-imagemin"),
-	rename = require("gulp-rename"),
-	clean = require("gulp-clean"),
-	concat = require("gulp-concat"),
-	notify = require("gulp-notify"),
-	cache = require("gulp-cache"),
-	livereload = require("gulp-livereload"),
-	lr = require("tiny-lr"),
-	server = lr();
+// let minifycss = require("gulp-minify-css"),
+// 	jshint = require("gulp-jshint");
+// // Styles
+// task("styles", function () {
+// 	return gulp
+// 		.src("src/styles/main.scss")
+// 		.pipe(sass({ style: "expanded" }))
+// 		.pipe(
+// 			autoprefixer(
+// 				"last 2 version",
+// 				"safari 5",
+// 				"ie 8",
+// 				"ie 9",
+// 				"opera 12.1",
+// 				"ios 6",
+// 				"android 4"
+// 			)
+// 		)
+// 		.pipe(dest("dist/styles"))
+// 		.pipe(rename({ suffix: ".min" }))
+// 		.pipe(minifycss())
+// 		.pipe(dest("dist/styles"))
+// 		.pipe(notify({ message: "Styles task complete" }));
+// });
 
-// Styles
-gulp.task("styles", function () {
-	return gulp
-		.src("src/styles/main.scss")
-		.pipe(sass({ style: "expanded" }))
-		.pipe(
-			autoprefixer(
-				"last 2 version",
-				"safari 5",
-				"ie 8",
-				"ie 9",
-				"opera 12.1",
-				"ios 6",
-				"android 4"
-			)
-		)
-		.pipe(gulp.dest("dist/styles"))
-		.pipe(rename({ suffix: ".min" }))
-		.pipe(minifycss())
-		.pipe(livereload(server))
-		.pipe(gulp.dest("dist/styles"))
-		.pipe(notify({ message: "Styles task complete" }));
-});
-
-// Scripts
-gulp.task("scripts", function () {
-	return gulp
-		.src("src/scripts/**/*.js")
-		.pipe(jshint(".jshintrc"))
-		.pipe(jshint.reporter("default"))
-		.pipe(concat("main.js"))
-		.pipe(gulp.dest("dist/scripts"))
-		.pipe(rename({ suffix: ".min" }))
-		.pipe(uglify())
-		.pipe(livereload(server))
-		.pipe(gulp.dest("dist/scripts"))
-		.pipe(notify({ message: "Scripts task complete" }));
-});
-
-// Images
-gulp.task("images", function () {
-	return gulp
-		.src("src/images/**/*")
-		.pipe(
-			cache(
-				imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })
-			)
-		)
-		.pipe(livereload(server))
-		.pipe(gulp.dest("dist/images"))
-		.pipe(notify({ message: "Images task complete" }));
-});
-
-// Clean
-gulp.task("clean", function () {
-	return gulp
-		.src(["dist/styles", "dist/scripts", "dist/images"], { read: false })
-		.pipe(clean());
-});
-
-// Default task
-gulp.task("default", ["clean"], function () {
-	gulp.start("styles", "scripts", "images");
-});
-
-// Watch
-gulp.task("watch", function () {
-	// Listen on port 35729
-	server.listen(35729, function (err) {
-		if (err) {
-			return console.log(err);
-		}
-
-		// Watch .scss files
-		gulp.watch("src/styles/**/*.scss", ["styles"]);
-
-		// Watch .js files
-		gulp.watch("src/scripts/**/*.js", ["scripts"]);
-
-		// Watch image files
-		gulp.watch("src/images/**/*", ["images"]);
-	});
-});
-
-// <!-- livereload -->
-var lr = require("tiny-lr")();
-
-function notify(lr, root) {
-	return function (event) {
-		var fname = require("path").relative(root, event.path);
-		lr.changed({ body: { files: [fname] } });
-	};
-}
-
-gulp.task("livereload", function () {
-	lr.listen(35729);
-	gulp.watch("public/**/*", notify(lr, __dirname + "/public"));
-});
-
-// Express
-app.use(require("connect-livereload")());
-//<script>document.write('<script src="'+(location.protocol||'http:')+'//'+(location.hostname||'localhost')+':35729/livereload.js?snipver=1"><\/scr'+'ipt>')</script>
+// // Scripts
+// task("scripts", function () {
+// 	return gulp
+// 		.src("src/scripts/**/*.js")
+// 		.pipe(jshint(".jshintrc"))
+// 		.pipe(jshint.reporter("default"))
+// 		.pipe(concat("main.js"))
+// 		.pipe(dest("dist/scripts"))
+// 		.pipe(rename({ suffix: ".min" }))
+// 		.pipe(uglify())
+// 		.pipe(dest("dist/scripts"))
+// 		.pipe(notify({ message: "Scripts task complete" }));
+// });
