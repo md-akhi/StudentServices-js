2ffb657784bdce12928d2ce9afe25c858d75b21a
Mohammad Amanalikhani <md.akhi.ir@gmail.com>
2021-12-13 21:46:41 +0330

    chore(jsx): hook هزینه‌ها

--- src/views/customer/dashboard/frelanser/request_add.jsx
+++ src/views/customer/dashboard/frelanser/request_add.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import ReactDOMServer from "react-dom/server";
+import React, { Component, useState, useReducer, useRef } from "react";
 //import Axios from "axios";
 
 import * as dataFrelanser from "../../../data/frelanser.js";
@@ -10,15 +9,21 @@ import MainSidebarLayout from "../../layouts/mainSidebar.jsx";
 import NavbarLayout from "../../layouts/navbar.jsx";
 
 export default (props) => {
-	let data = props.data;
-	data.invoice = { items: "", text: "" };
+	const { data = null, isEdit = false } = props;
+	const {
+		userId = null,
+		projectId = null,
+		description = null,
+		duration = null,
+		invoice = [],
+		createdAt = null,
+	} = data;
 	return (
 		<BodyLayout className="hold-transition sidebar-mini layout-fixed">
 			<NavbarLayout NavbarLinks={dataFrelanser.linkNavUp}></NavbarLayout>
 			<MainSidebarLayout
 				Data={dataFrelanser.menuSidbarRight}
 			></MainSidebarLayout>
-
 			{/* Content Wrapper. Contains page content */}
 			<div className="content-wrapper">
 				{/* Content Header (Page header) */}
@@ -34,7 +39,7 @@ export default (props) => {
 					<div className="container-fluid">
 						{/* Main content */}
 						<section className="content">
-							<form action={props.isEdit ? "./edit" : "./add"} method="post">
+							<form action={isEdit ? "./edit" : "./add"} method="post">
 								<div className="row">
 									<div className="col-md-6">
 										<div className="card card-primary">
@@ -51,8 +56,20 @@ export default (props) => {
 														className="form-control"
 														rows="4"
 														name="description"
-														defaultValue={props.isEdit ? data.description : ""}
+														defaultValue={description}
 													></textarea>
+												</div>
+											</div>
+											{/* /.card-body */}
+										</div>
+										<div className="card card-secondary">
+											<div className="card-header">
+												<h3 className="card-title">invoce</h3>
+											</div>
+											<div className="card-body">
+												<div className="form-group">
+													<label htmlFor="ShowInvoce"> invoce</label>
+													<InvocesApp data={invoice}></InvocesApp>
 												</div>
 											</div>
 											{/* /.card-body */}
@@ -66,9 +83,6 @@ export default (props) => {
 											</div>
 											<div className="card-body">
 												<div className="form-group">
-													<label htmlFor="inputamount">Total amount</label>
-												</div>
-												<div className="form-group">
 													<label htmlFor="inputEstimatedDuration">
 														Estimated project duration
 													</label>
@@ -77,22 +91,8 @@ export default (props) => {
 														id="inputEstimatedDuration"
 														className="form-control"
 														name="duration"
-														defaultValue={
-															props.isEdit ? data.estimatedDuration : ""
-														}
+														defaultValue={duration}
 													/>
-												</div>
-											</div>
-											{/* /.card-body */}
-										</div>
-										<div className="card card-secondary">
-											<div className="card-header">
-												<h3 className="card-title">invoce</h3>
-											</div>
-											<div className="card-body">
-												<div className="form-group">
-													<label htmlFor="ShowInvoce"> invoce</label>
-													<TodoApp invoice={data.invoice}></TodoApp>
 												</div>
 											</div>
 											{/* /.card-body */}
@@ -108,19 +108,15 @@ export default (props) => {
 										<input
 											type="submit"
 											defaultValue={
-												props.isEdit ? "Update Request" : "Create Request"
+												isEdit ? "Update Request" : "Create Request"
 											}
 											className="btn btn-success float-right"
 										/>
-										<input
-											type="hidden"
-											name="userId"
-											defaultValue={props.user}
-										/>
+										<input type="hidden" name="userId" defaultValue={userId} />
 										<input
 											type="hidden"
 											name="projectId"
-											defaultValue={props.project}
+											defaultValue={projectId}
 										/>
 									</div>
 								</div>
@@ -134,64 +130,122 @@ export default (props) => {
 			</div>
 			{/* /.content-wrapper */}
 			<FooterLayout></FooterLayout>
-
 			{/* Control Sidebar */}
 			<aside className="control-sidebar control-sidebar-dark">
 				{/* Control sidebar content goes here */}
 			</aside>
 			{/* /.control-sidebar */}
 			{/* ./wrapper */}
+			{/* <script
+				dangerouslySetInnerHTML={{
+					__html: "window.__PRELOADED_STATE__ =" + JSON.stringify(props),
+				}}
+			></script> */}
 		</BodyLayout>
 	);
 };
 
-class TodoList extends Component {
-	render() {
-		var i = 0;
-		var createItem = function (itemText) {
-			return <li key={i++}>{itemText}</li>;
-		};
-		return <ul>{this.props.items.map(createItem)}</ul>;
-	}
-}
+function InvocesApp(props) {
+	const { data = [] } = props;
+	const [items, setItems] = useState(data);
+	const [item, setItem] = useState({
+		description: "",
+		amount: 0,
+	});
 
-class TodoApp extends Component {
-	constructor(props) {
-		super(props);
-		this.onChange = this.onChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.state = props.invoice;
-	}
-	onChange(e) {
-		this.setState({ text: e.target.value });
-	}
-	handleSubmit(e) {
+	const onChange = (e) => {
+		let { description = "", amount = 0 } = item;
+		if (e.target.name == "idescription") {
+			description = e.target.value;
+		} else if (e.target.name == "iamount") {
+			amount = e.target.value;
+		}
+		setItem({
+			description: description,
+			amount: amount,
+		});
+	};
+
+	const handleRemove = (e, id) => {
+		const itms = items.filter((item, index) => {
+			if (id !== index) return item;
+		});
+		// Update state with filter
+		setItems([...itms]);
+	};
+
+	const handleEdit = (e, id, description, amount) => {
+		handleRemove(e, id);
+		setItem({
+			description: description,
+			amount: amount,
+		});
+	};
+
+	const handleSubmit = (e) => {
 		e.preventDefault();
-		var nextItems = this.state.items.concat([this.state.text]);
-		var nextText = "";
-		this.setState({ items: nextItems, text: nextText });
-	}
-	render() {
-		return (
-			<div className="container">
-				<h3>TODO List</h3>
-				<TodoList items={this.state.items} />
-				<form className="form-inline" onSubmit={this.handleSubmit}>
-					<div className="form-group">
-						<input
-							type="text"
-							className="form-control"
-							placeholder="Write task"
-							onChange={this.onChange}
-							value={this.state.text}
-						/>
-						&nbsp;
-					</div>
-					<a className="btn btn-primary">
-						{"Add #" + (this.state.items.length + 1)}
-					</a>
-				</form>
+		setItems([...items, item]);
+		setItem({ description: "", amount: 0 });
+	};
+
+	return (
+		<div>
+			<div className="form-group">
+				<label htmlFor="inputamount">Total amount:</label>
+				{items.reduce((total, item) => total + item.amount, 0)}
 			</div>
-		);
-	}
+			<ul>
+				{items.map((item, index) => {
+					const { description = "", amount = 0 } = item;
+					return (
+						<li key={index.toString()} onClick={(e) => handleRemove(e, index)}>
+							{index + 1}:{description} - ${amount}{" "}
+							<a
+								className="btn btn-primary"
+								onClick={(e) => handleEdit(e, index, description, amount)}
+							>
+								{"Edit #" + (index + 1)}
+							</a>
+							<a
+								className="btn btn-primary"
+								onClick={(e) => handleRemove(e, index)}
+							>
+								*
+							</a>
+							{/* <a onClick={this.props.remove(item._id)}> [*] </a> */}
+						</li>
+					);
+				})}
+				<li>
+					{items.length + 1}: {item.description} - ${item.amount}
+				</li>
+			</ul>
+			<div className="form-group">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Write task"
+					name="idescription"
+					onChange={onChange}
+					value={item.description}
+				/>
+				<input
+					type="number"
+					className="form-control"
+					name="iamount"
+					onChange={onChange}
+					value={item.amount}
+				/>
+				&nbsp;
+				<input
+					type="hidden"
+					name="invoice[]"
+					defaultValue={"" + JSON.stringify(items) + ""}
+				/>
+			</div>
+			<a className="btn btn-primary" onClick={handleSubmit}>
+				{"Add #" + (items.length + 1)}
+			</a>
+		</div>
+	);
 }
